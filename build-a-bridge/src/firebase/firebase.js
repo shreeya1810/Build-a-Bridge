5d56dc1b40cf1dc024b316325cb159fed430faf6
Shreeya Gokhale <shreeyagokhale@Shreeyas-MacBook-Air.local>
2024-10-04 20:50:01 +0530

    made changes to firebase.js

--- build-a-bridge/src/firebase/firebase.js
+++ build-a-bridge/src/firebase/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Correct access to environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCkVT1si1I7t7j6ih5Gu1_aC2GO5RKv0iA",
-  authDomain: "build-a-bridge-e165e.firebaseapp.com",
-  projectId: "build-a-bridge-e165e",
-  storageBucket: "build-a-bridge-e165e.appspot.com",
-  messagingSenderId: "355030794692",
-  appId: "1:355030794692:web:ffa3e9c9aef93b594e6491",
-  measurementId: "G-4NG389EQ9Z"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,3 +18,26 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyABakzfaj4pcxXPltNb17jbSEetrhA-ZLk",
+//   authDomain: "build-a-bridge-f2068.firebaseapp.com",
+//   projectId: "build-a-bridge-f2068",
+//   storageBucket: "build-a-bridge-f2068.appspot.com",
+//   messagingSenderId: "878063170236",
+//   appId: "1:878063170236:web:2f33ea96c710e847aad6b9",
+//   measurementId: "G-TTZBT2VJ7E"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// export { auth, db };
